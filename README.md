bdc7bb8bb5429acf7bd9ef4610a09a22bf3e2e7b
ErolBirced <55743781+ErolBirced@users.noreply.github.com>
2021-04-22 11:58:44 +0100

    Connected folder to github repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# set08101
-Web Technologies 20/21 - Lab Work
---
+++ script.js
@@ -0,0 +1,35 @@
+const openLeft = document.getElementById("open");
+const closeLeft = document.getElementById("close");
+const openRight = document.getElementById("open2");
+const closeRight = document.getElementById("close2");
+const container = document.querySelector(".container");
+const circleContainer = document.querySelector(".circle-container");
+const circleContainer2 = document.querySelector(".circle-container2");
+const goBack = document.getElementById("goBack");
+const quit = document.getElementById("quit");
+const save = document.getElementById("save");
+const refresh = document.getElementById("refresh");
+
+goBack.addEventListener("click", () => history.back());
+
+quit.addEventListener("click", () => (window.location = "../index.html"));
+
+refresh.addEventListener("click", () => location.reload());
+
+// save.addEventListener("click", () => )
+
+closeLeft.addEventListener("click", () =>
+  container.classList.remove("show-nav")
+);
+
+closeRight.addEventListener("click", () =>
+  container.classList.remove("show-nav2")
+);
+
+openLeft.addEventListener("click", () => {
+  container.classList.add("show-nav");
+});
+
+openRight.addEventListener("click", () => {
+  container.classList.add("show-nav2");
+});
